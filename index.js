c7fa48d19b739c0de53ea4fe8e0651942e5e7aba
Chandler Nova <chandlersnova@gmail.com>
2023-10-17 10:42:47 -0700

    Added Player class with tests -Tests for changing turns -Tests for having missed a target

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ class GameBoard {
     this.destroyer = "";
     this.submarine = "";
     this.patrolBoat = "";
+    this.sunkShips = 0
   }
 
   placeShip(name, size, direction, location) {
@@ -57,9 +58,69 @@ class GameBoard {
       }
     }
   }
+
+  receiveAttack(location) {
+    if (this.gameBoard[location] === null) {
+      this.gameBoard[location] = "miss"
+    } else if (this.gameBoard[location] !== "hit") {
+      if (this.gameBoard[location] === "carrier") {
+        this.carrier.getHit()
+        this.carrier.isSunk()
+        if (this.carrier.sunk === true) this.sunkShips++
+      }
+      if (this.gameBoard[location] === "battleship") {
+        this.battleship.getHit()
+        this.battleship.isSunk()
+        if (this.battleship.sunk === true) this.sunkShips++
+      }
+      if (this.gameBoard[location] === "destroyer") {
+        this.destroyer.getHit()
+        this.destroyer.isSunk()
+        if (this.destroyer.sunk === true) this.sunkShips++
+      }
+      if (this.gameBoard[location] === "submarine") {
+        this.submarine.getHit()
+        this.submarine.isSunk()
+        if (this.submarine.sunk === true) this.sunkShips++
+      }
+      if (this.gameBoard[location] === "patrolBoat") {
+        this.patrolBoat.getHit()
+        this.patrolBoat.isSunk()
+        if (this.patrolBoat.sunk === true) this.sunkShips++
+      }
+      this.gameBoard[location] = "hit"
+    }
+  }
+
+  allShipsSunk() {
+    if (this.sunkShips >= 5) return true
+    else return false
+  }
+}
+
+class Player {
+  constructor() {
+    this.turn = "player"
+  }
+
+  playerTurn(location, computerBoard) {
+    if (this.turn === "player") {
+      computerBoard.receiveAttack(location)
+    }
+    this.turn = "computer"
+  }
+
+  computerTurn(location, gameBoard) {
+    if (this.turn === "computer") {
+      gameBoard.receiveAttack(location)
+    }
+    this.turn = "player"
+  }
+
 }
 
 module.exports = {
   Ship,
   GameBoard,
+  Player,
 };
--- index.test.js
+++ index.test.js
@@ -1,4 +1,4 @@
-const { Ship, GameBoard } = require("./index.js");
+const { Ship, GameBoard, Player } = require("./index.js");
 
 // Testing ship.getHit and ship.isSunk
 test("Does ship.getHits change hits 1", () => {
@@ -605,6 +605,586 @@ test("Place 5 ships on board (mixed directions)", () => {
     null,
     null,
     null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    "carrier",
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    "carrier",
+    null,
+    null,
+    "destroyer",
+    "destroyer",
+    "destroyer",
+    null,
+    null,
+    null,
+    null,
+    "carrier",
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    "carrier",
+    null,
+    null,
+    null,
+    null,
+    null,
+    "patrolBoat",
+    null,
+    null,
+    null,
+    "carrier",
+    null,
+    null,
+    null,
+    null,
+    null,
+    "patrolBoat",
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    "battleship",
+    "battleship",
+    "battleship",
+    "battleship",
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+  ]);
+});
+
+
+test("Does recieivng a miss get recorded", () => {
+  let gameBoard = new GameBoard();
+  gameBoard.placeShip("carrier", 5, "y", 22);
+  gameBoard.placeShip("battleship", 4, "x", 83);
+  gameBoard.placeShip("destroyer", 3, "x", 35);
+  gameBoard.placeShip("submarine", 3, "x", 5);
+  gameBoard.placeShip("patrolBoat", 2, "y", 58);
+  gameBoard.receiveAttack(10)
+  expect(gameBoard.gameBoard).toEqual([
+    null,
+    null,
+    null,
+    null,
+    null,
+    "submarine",
+    "submarine",
+    "submarine",
+    null,
+    null,
+    "miss",
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    "carrier",
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    "carrier",
+    null,
+    null,
+    "destroyer",
+    "destroyer",
+    "destroyer",
+    null,
+    null,
+    null,
+    null,
+    "carrier",
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    "carrier",
+    null,
+    null,
+    null,
+    null,
+    null,
+    "patrolBoat",
+    null,
+    null,
+    null,
+    "carrier",
+    null,
+    null,
+    null,
+    null,
+    null,
+    "patrolBoat",
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    "battleship",
+    "battleship",
+    "battleship",
+    "battleship",
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+  ]);
+});
+
+test("Does recieivng a hit get recorded", () => {
+  let gameBoard = new GameBoard();
+  gameBoard.placeShip("carrier", 5, "y", 22);
+  gameBoard.placeShip("battleship", 4, "x", 83);
+  gameBoard.placeShip("destroyer", 3, "x", 35);
+  gameBoard.placeShip("submarine", 3, "x", 5);
+  gameBoard.placeShip("patrolBoat", 2, "y", 58);
+  gameBoard.receiveAttack(6)
+  expect(gameBoard.gameBoard).toEqual([
+    null,
+    null,
+    null,
+    null,
+    null,
+    "submarine",
+    "hit",
+    "submarine",
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    "carrier",
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    "carrier",
+    null,
+    null,
+    "destroyer",
+    "destroyer",
+    "destroyer",
+    null,
+    null,
+    null,
+    null,
+    "carrier",
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    "carrier",
+    null,
+    null,
+    null,
+    null,
+    null,
+    "patrolBoat",
+    null,
+    null,
+    null,
+    "carrier",
+    null,
+    null,
+    null,
+    null,
+    null,
+    "patrolBoat",
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    "battleship",
+    "battleship",
+    "battleship",
+    "battleship",
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+  ]);
+});
+
+test("Does recieivng a hit change the ships hit count?", () => {
+  let gameBoard = new GameBoard();
+  gameBoard.placeShip("carrier", 5, "y", 22);
+  gameBoard.placeShip("battleship", 4, "x", 83);
+  gameBoard.placeShip("destroyer", 3, "x", 35);
+  gameBoard.placeShip("submarine", 3, "x", 5);
+  gameBoard.placeShip("patrolBoat", 2, "y", 58);
+  gameBoard.receiveAttack(6)
+  expect(gameBoard.submarine.hits).toBe(1);
+});
+
+test("Does recieivng enough hits change the ships sunk variable?", () => {
+  let gameBoard = new GameBoard();
+  gameBoard.placeShip("carrier", 5, "y", 22);
+  gameBoard.placeShip("battleship", 4, "x", 83);
+  gameBoard.placeShip("destroyer", 3, "x", 35);
+  gameBoard.placeShip("submarine", 3, "x", 5);
+  gameBoard.placeShip("patrolBoat", 2, "y", 58);
+  gameBoard.receiveAttack(6)
+  gameBoard.receiveAttack(5)
+  gameBoard.receiveAttack(7)
+  expect(gameBoard.submarine.sunk).toBe(true);
+});
+
+test("Does sinking a ship change the sunkShips variable?", () => {
+  let gameBoard = new GameBoard();
+  gameBoard.placeShip("carrier", 5, "y", 22);
+  gameBoard.placeShip("battleship", 4, "x", 83);
+  gameBoard.placeShip("destroyer", 3, "x", 35);
+  gameBoard.placeShip("submarine", 3, "x", 5);
+  gameBoard.placeShip("patrolBoat", 2, "y", 58);
+  gameBoard.receiveAttack(6)
+  gameBoard.receiveAttack(5)
+  gameBoard.receiveAttack(7)
+  expect(gameBoard.sunkShips).toBe(1);
+});
+
+test("Does hitting the same place twice change hits count?", () => {
+  let gameBoard = new GameBoard();
+  gameBoard.placeShip("carrier", 5, "y", 22);
+  gameBoard.placeShip("battleship", 4, "x", 83);
+  gameBoard.placeShip("destroyer", 3, "x", 35);
+  gameBoard.placeShip("submarine", 3, "x", 5);
+  gameBoard.placeShip("patrolBoat", 2, "y", 58);
+  gameBoard.receiveAttack(6)
+  gameBoard.receiveAttack(6)
+  expect(gameBoard.submarine.hits).toBe(1);
+});
+
+test("Does gameboard report when all ships sunk (false)", () => {
+  let gameBoard = new GameBoard();
+  gameBoard.placeShip("carrier", 5, "y", 22);
+  gameBoard.placeShip("battleship", 4, "x", 83);
+  gameBoard.placeShip("destroyer", 3, "x", 35);
+  gameBoard.placeShip("submarine", 3, "x", 5);
+  gameBoard.placeShip("patrolBoat", 2, "y", 58);
+  gameBoard.receiveAttack(5)
+  gameBoard.receiveAttack(6)
+  gameBoard.receiveAttack(7)
+  expect(gameBoard.allShipsSunk()).toBe(false);
+});
+
+test("Does gameboard report when all ships sunk (true)", () => {
+  let gameBoard = new GameBoard();
+  gameBoard.placeShip("carrier", 5, "y", 22);
+  gameBoard.placeShip("battleship", 4, "x", 83);
+  gameBoard.placeShip("destroyer", 3, "x", 35);
+  gameBoard.placeShip("submarine", 3, "x", 5);
+  gameBoard.placeShip("patrolBoat", 2, "y", 58);
+  gameBoard.receiveAttack(58)
+  gameBoard.receiveAttack(68)
+  gameBoard.receiveAttack(5)
+  gameBoard.receiveAttack(6)
+  gameBoard.receiveAttack(7)
+  gameBoard.receiveAttack(35)
+  gameBoard.receiveAttack(36)
+  gameBoard.receiveAttack(37)
+  gameBoard.receiveAttack(83)
+  gameBoard.receiveAttack(84)
+  gameBoard.receiveAttack(85)
+  gameBoard.receiveAttack(86)
+  gameBoard.receiveAttack(22)
+  gameBoard.receiveAttack(32)
+  gameBoard.receiveAttack(42)
+  gameBoard.receiveAttack(52)
+  gameBoard.receiveAttack(62)
+  expect(gameBoard.allShipsSunk()).toBe(true);
+});
+
+test("After Player takes turn the turn switches", () => {
+  let gameBoard = new GameBoard();
+  gameBoard.placeShip("carrier", 5, "y", 22);
+  gameBoard.placeShip("battleship", 4, "x", 83);
+  gameBoard.placeShip("destroyer", 3, "x", 35);
+  gameBoard.placeShip("submarine", 3, "x", 5);
+  gameBoard.placeShip("patrolBoat", 2, "y", 58);
+  let computerBoard = new GameBoard();
+  let player = new Player();
+  player.playerTurn(6, computerBoard)
+  expect(player.turn).toBe("computer");
+});
+
+test("After Computer takes turn the turn switches", () => {
+  let gameBoard = new GameBoard();
+  gameBoard.placeShip("carrier", 5, "y", 22);
+  gameBoard.placeShip("battleship", 4, "x", 83);
+  gameBoard.placeShip("destroyer", 3, "x", 35);
+  gameBoard.placeShip("submarine", 3, "x", 5);
+  gameBoard.placeShip("patrolBoat", 2, "y", 58);
+  let computerBoard = new GameBoard();
+  let player = new Player();
+  player.playerTurn(6, computerBoard)
+  player.computerTurn(10, gameBoard)
+  expect(player.turn).toBe("player");
+});
+
+test("Does Player getting a miss get recorded", () => {
+  let gameBoard = new GameBoard();
+  gameBoard.placeShip("carrier", 5, "y", 22);
+  gameBoard.placeShip("battleship", 4, "x", 83);
+  gameBoard.placeShip("destroyer", 3, "x", 35);
+  gameBoard.placeShip("submarine", 3, "x", 5);
+  gameBoard.placeShip("patrolBoat", 2, "y", 58);
+  let computerBoard = new GameBoard();
+  computerBoard.placeShip("carrier", 5, "x", 53);
+  computerBoard.placeShip("battleship", 4, "y", 18);
+  computerBoard.placeShip("destroyer", 3, "x", 21);
+  computerBoard.placeShip("submarine", 3, "y", 61);
+  computerBoard.placeShip("patrolBoat", 2, "x", 86);
+  let player = new Player();
+  player.playerTurn(10, computerBoard)
+  expect(computerBoard.gameBoard).toEqual([
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    "miss",
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    "battleship",
+    null,
+    null,
+    "destroyer",
+    "destroyer",
+    "destroyer",
+    null,
+    null,
+    null,
+    null,
+    "battleship",
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    "battleship",
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    "battleship",
+    null,
+    null,
+    null,
+    null,
+    "carrier",
+    "carrier",
+    "carrier",
+    "carrier",
+    "carrier",
+    null,
+    null,
+    null,
+    "submarine",
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    "submarine",
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    "submarine",
+    null,
+    null,
+    null,
+    null,
+    "patrolBoat",
+    "patrolBoat",
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+  ]);
+});
+
+test("Does Computer getting a miss get recorded", () => {
+  let gameBoard = new GameBoard();
+  gameBoard.placeShip("carrier", 5, "y", 22);
+  gameBoard.placeShip("battleship", 4, "x", 83);
+  gameBoard.placeShip("destroyer", 3, "x", 35);
+  gameBoard.placeShip("submarine", 3, "x", 5);
+  gameBoard.placeShip("patrolBoat", 2, "y", 58);
+  let computerBoard = new GameBoard();
+  computerBoard.placeShip("carrier", 5, "x", 53);
+  computerBoard.placeShip("battleship", 4, "y", 18);
+  computerBoard.placeShip("destroyer", 3, "x", 21);
+  computerBoard.placeShip("submarine", 3, "y", 61);
+  computerBoard.placeShip("patrolBoat", 2, "x", 86);
+  let player = new Player();
+  player.playerTurn(10, computerBoard)
+  player.computerTurn(10, gameBoard)
+  expect(gameBoard.gameBoard).toEqual([
+    null,
+    null,
+    null,
+    null,
+    null,
+    "submarine",
+    "submarine",
+    "submarine",
+    null,
+    null,
+    "miss",
     null,
     null,
     null,
